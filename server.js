af1354cdcf782376a65cb01298f164b549642824
kihBootCamp <kimberleyheuer@hotmail.com>
2021-03-12 22:53:44 -0600

    aggregation added / last workout not working for cardio

--- server.js
+++ server.js
@@ -29,11 +29,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 
 app.get("/api/workouts", (req, res) => {
   db.Workout.find({})
-    .then (data => {
+    .then(data => {
       console.log("finding one", data);
       res.json(data);
     })
-    .catch (err => {
+    .catch(err => {
       res.json(err);
     })
 })
@@ -42,13 +42,13 @@ app.get("/api/workouts", (req, res) => {
 // create a workout id and store in dbs, the excercise object is empty
 // {body} same as req and req.bdoy
 
-app.post("/api/workouts", ({body}, res) => {
+app.post("/api/workouts", ({ body }, res) => {
   db.Workout.create(body)
-    .then (data => {
+    .then(data => {
       console.log("new wrkout = ", data);
       res.json(data);
     })
-    .catch (err => {
+    .catch(err => {
       res.json(err);
     })
 })
@@ -59,33 +59,42 @@ app.post("/api/workouts", ({body}, res) => {
 
 app.put("/api/workouts/:id", (req, res) => {
   console.log(req.body);
-  //const newWorkout = db.Workout.create(req.body);
+
   const id = req.params.id;
-  const newExcercise = {exercises: req.body};
-  const filter = {id: req.params.id};
+  const newExcercise = { exercises: req.body }; // this has too many brackets
+  const filter = { id: req.params.id };
 
   console.log(id);
   console.log("new excercise: ", newExcercise);
 
-  db.Workout.findByIdAndUpdate(id, {$push: {exercises: req.body }}, {new: true})
-  //db.Workout.findByIdAndUpdate(filter,  { $push: newExcercise}, {new: true})  
-    .then (data => {
+  db.Workout.findByIdAndUpdate(id, { $push: { exercises: req.body } }, { new: true })
+    //db.Workout.findByIdAndUpdate(filter,  { $push: newExcercise}, {new: true})  
+    .then(data => {
       res.json(data);
     })
-    .catch (err => {
+    .catch(err => {
       res.json(err);
     })
 })
 
 app.get("/api/workouts/range", (req, res) => {
-  db.Workout.find({})
-    .then (data => {
-      res.json(data);
-    })
-    .catch (err => {
+  db.Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+        totalDistance: { $sum: "$exercises.distance" }
+      }
+    }
+  ])
+  .then(data => {
+    console.log("aggreatation: ", data);
+     res.json(data);
+  })
+  .catch(err => {
       res.json(err);
-    })
+  })
 })
+
 
 app.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "exercise.html"))
@@ -97,5 +106,5 @@ app.get("/stats", (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+  console.log(`App running on port ${PORT}!`);
+});
